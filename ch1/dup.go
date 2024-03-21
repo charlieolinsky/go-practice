fc3e3b25ed22a65006472e1148d9fe860b01503c
charlieolinsky <charlesolinsky@gmail.com>
2024-03-21 12:23:25 -0400

    Duplicate lines #2

--- ch1/dup.go
+++ ch1/dup.go
@@ -8,7 +8,8 @@ import (
 
 func Dup() {
 	fmt.Println("\nFinding Duplicate Lines")
-	Dup1()
+	//Dup1()
+	Dup2()
 }
 
 // Dup1 prints the text of each line that appears more than 
@@ -32,3 +33,41 @@ func Dup1() {
 
 	fmt.Println("Counts Map: ", counts)
 }
+// This version can read from the standard input or from a seqeunce of named files
+// Note, there is a file called text.txt in /test to use
+func Dup2() {
+	counts := make(map[string]int)
+	//Takes file name from Args
+	files := os.Args[1:]	
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file) // os.Open() returns both an open file (*os.File), the second is an error 
+			
+			//if something went wrong display the error	
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "ERR_DUP2: %v\n", err)
+				continue
+			}
+
+			countLines(f, counts)
+			f.Close() //closes the file and releases any resources
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan(){
+		counts[input.Text()]++
+	}
+	//NOTE: ignoring potential errors from input.Err()
+}
+
+
