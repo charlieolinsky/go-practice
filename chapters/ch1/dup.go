a332239cc2e246219ceed7335c27f34aba69b408
charlieolinsky <charlesolinsky@gmail.com>
2024-03-24 15:45:44 -0400

    dup3

--- chapters/ch1/dup.go
+++ chapters/ch1/dup.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func Dup() {
 	fmt.Println("\nFinding Duplicate Lines")
 	//Dup1()
-	Dup2()
+	//Dup2()
+	Dup3()
 }
 
 // Dup1 prints the text of each line that appears more than
@@ -69,4 +71,25 @@ func countLines(f *os.File, counts map[string]int) {
 		counts[input.Text()]++
 	}
 	//NOTE: ignoring potential errors from input.Err()
+}
+
+//Here we read the entire input into memory in one shot, split it into lines, and the process.
+func Dup3() {
+	counts := make(map[string]int)
+	for _, fileName := range os.Args[1:] {
+		data, err := os.ReadFile(fileName)
+		if err != nil{
+			fmt.Fprintf(os.Stderr, "ERRDUP3: %v\n", err) 
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+
 }
